20497222dcf64693ebe14ca2e20b55953a74013c
Mariano Paciaroni <marianopaciaroni@gmail.com>
2025-06-04 21:12:34 -0300

    Configura firebaseConfig.js para usar variables de entorno y agrega .env de desarrollo

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK8Rmtti1TrKRCR_sUzhncm4ebk7OoUXs",
-  authDomain: "mruh2-398d6.firebaseapp.com",
-  databaseURL: "https://mruh2-398d6.firebaseio.com",
-  projectId: "mruh2-398d6",
-  storageBucket: "mruh2-398d6.firebasestorage.app",
-  messagingSenderId: "274684476540",
-  appId: "1:274684476540:web:5dc96795cee6e8c81d5df9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
